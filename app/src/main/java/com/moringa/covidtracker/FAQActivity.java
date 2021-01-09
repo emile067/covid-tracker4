bb08789bcb211b36c2f2fb503e494428f9787278
emile kamana <emileizere@gmail.com>
2021-01-09 22:23:30 +0200

    create different sumptoms arrays

--- app/src/main/java/com/moringa/covidtracker/FAQActivity.java
+++ app/src/main/java/com/moringa/covidtracker/FAQActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FAQActivity extends AppCompatActivity {
+    private String[] mostCommonSymptoms = new String[] {"fever", "dry cough", "tiredness"};
+    private String[] lessCommonSymptoms = new String[] {"aches and pains", "sore throat", "diarrhoea","conjunctivitis","headache","loss of taste or smell","a rash on skin, or discolouration of fingers or toes"};
+    private String[] seriousSymptoms = new String[] {"difficulty breathing or shortness of breath","chest pain or pressure", "loss of speech or movement"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
